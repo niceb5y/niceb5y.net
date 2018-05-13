cb8677dffbcc56ad666f99afd4417ab42d3d586e
Seungho Kim <niceb5y@gmail.com>
2018-05-13 22:51:22 +0900

    update service workers

--- src/index.js
+++ src/index.js
@@ -8,41 +8,9 @@ ReactDOM.render(
   document.getElementById('root')
 )
 
-const isLocalhost = Boolean(
-  window.location.hostname === "localhost" ||
-  window.location.hostname === "[::1]" ||
-  window.location.hostname.match(/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/)
-)
-
-export default function register() {
-  if (process.env.NODE_ENV === "production" && "serviceWorker" in navigator) {
-    const publicUrl = new URL("/", window.location)
-    if (publicUrl.origin !== window.location.origin) return
-    window.addEventListener("load", () => {
-      const swUrl = `/service-worker.js`
-      if (isLocalhost) {
-        checkValidServiceWorker(swUrl)
-      } else {
-        navigator.serviceWorker.register(swUrl)
-      }
-    })
-  }
-}
-
-function checkValidServiceWorker(swUrl) {
-  fetch(swUrl)
-  .then(response => {
-    if (
-      response.status === 404 ||
-      response.headers.get("content-type").indexOf("javascript") === -1
-    ) {
-      navigator.serviceWorker.ready.then(registration => {
-        registration.unregister().then(() => {
-          window.location.reload()
-        })
-      })
-    } else {
-      navigator.serviceWorker.register(swUrl)
-    }
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    const swUrl = `/service-worker.js`
+    navigator.serviceWorker.register(swUrl)
   })
 }
