6ca615b6d338d11c226c39b263cb18ec550259d0
Seungho Kim <niceb5y@gmail.com>
2018-05-13 22:42:27 +0900

    register serviceworker

--- src/index.js
+++ src/index.js
@@ -7,3 +7,42 @@ ReactDOM.render(
   <App />,
   document.getElementById('root')
 )
+
+const isLocalhost = Boolean(
+  window.location.hostname === "localhost" ||
+  window.location.hostname === "[::1]" ||
+  window.location.hostname.match(/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/)
+)
+
+export default function register() {
+  if (process.env.NODE_ENV === "production" && "serviceWorker" in navigator) {
+    const publicUrl = new URL("/", window.location)
+    if (publicUrl.origin !== window.location.origin) return
+    window.addEventListener("load", () => {
+      const swUrl = `/service-worker.js`
+      if (isLocalhost) {
+        checkValidServiceWorker(swUrl)
+      } else {
+        navigator.serviceWorker.register(swUrl)
+      }
+    })
+  }
+}
+
+function checkValidServiceWorker(swUrl) {
+  fetch(swUrl)
+  .then(response => {
+    if (
+      response.status === 404 ||
+      response.headers.get("content-type").indexOf("javascript") === -1
+    ) {
+      navigator.serviceWorker.ready.then(registration => {
+        registration.unregister().then(() => {
+          window.location.reload()
+        })
+      })
+    } else {
+      navigator.serviceWorker.register(swUrl)
+    }
+  })
+}
