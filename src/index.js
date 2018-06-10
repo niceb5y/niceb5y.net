bb5aaf74e37a70d9fabb6b3951c86002e39035b7
Seungho Kim <niceb5y@gmail.com>
2018-06-10 21:31:20 +0900

    Disable registering service-worker in developement mode

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ ReactDOM.render(
   document.getElementById('root')
 )
 
-if ('serviceWorker' in navigator) {
+if ('serviceWorker' in navigator && process.env.NODE_ENV === 'production') {
   window.addEventListener('load', () => {
     const swUrl = `/sw.js`
     navigator.serviceWorker.register(swUrl)
